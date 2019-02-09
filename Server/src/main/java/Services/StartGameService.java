b5bdd90823e1afc88252ef545adf5eb2a296579c
sarah <sarahmykelle@gmail.com>
2019-02-09 14:24:36 -0700

    startgameservice

--- Server/src/main/java/Services/StartGameService.java
+++ Server/src/main/java/Services/StartGameService.java
@@ -1,8 +1,26 @@
 package Services;
 
+import Models.ActiveGame;
+import Models.IGame;
+import Models.MainModel;
+import Models.PendingGame;
+
 public class StartGameService implements Service {
+    MainModel model;
+
+    public StartGameService(){
+        model = MainModel.get();
+    }
+
     @Override
     public Object doService(Object... obj) {
+        String gameID = (String) obj[0];
+
+        IGame game = model.getGameList().get(gameID);
+        if(game != null && game.getClass().equals(PendingGame.class)){
+            ActiveGame ag = new ActiveGame(game);
+
+        }
         //get rid of pendingGame and change to activeGame
         //return valid message
         //-users need to all be listening for startGame
