ee3660621e6918b6252027d65e5ec7875d335cfa
ChandlerEady <chandler.eady@gmail.com>
2019-02-09 14:39:29 -0700

    Added server services

--- Server/src/main/java/Services/JoinGameService.java
+++ Server/src/main/java/Services/JoinGameService.java
@@ -1,13 +1,38 @@
 package Services;
 
+import Command.ErrorCommand;
+import Models.IGame;
+import Models.MainModel;
+import Models.PendingGame;
+import Models.Player;
+
 public class JoinGameService implements Service {
     //Params: player, gameID
     @Override
     public Object doService(Object... obj) {
+        Player player = (Player) obj[0];
+        String gameID = (String) obj[1];
+        IGame game = MainModel.get().getGameList().get(gameID);
+        if (game != null)
+        {
+            if (game.getClass() == PendingGame.class)
+            {
+                game.addPlayer(player);
+                System.out.println("Player added to game");
+                return true;
+            }
+            else
+            {
+                return new ErrorCommand("Game already started");
+            }
+        }
+        else
+        {
+            return new ErrorCommand("Game doesn't exist");
+        }
         //get game object(by id)
         //check that player is logged in and not already in game?
         //activeGame.addPlayer to game
         //return valid message
-        return null;
     }
 }
--- app/src/main/java/Command/ErrorCommand.java
+++ app/src/main/java/Command/ErrorCommand.java
@@ -3,17 +3,17 @@ package Command;
 import Services.ErrorService;
 
 public class ErrorCommand implements Command {
-    private String message;
+    private String error;
     private String ipAddress;
 
     @Override
     public void execute() {
         ErrorService errorService = new ErrorService();
-        errorService.doService(message, ipAddress);
+        errorService.doService(error, ipAddress);
     }
     public ErrorCommand(String message)
     {
-        this.message = message;
+        this.error = message;
         ipAddress = null;
     }
 }
