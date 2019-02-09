0d682b56b0e76dd09c8b85f2503a202a1521fb9e
ChandlerEady <chandler.eady@gmail.com>
2019-02-09 10:43:43 -0700

    Just the test main

--- app/src/main/java/Command/TestMain.java
+++ app/src/main/java/Command/TestMain.java
@@ -1,16 +1,22 @@
 package Command;
 
 
+import Models.Player;
+import Models.PlayerColorEnum;
+
 public class TestMain {
 
     public static void main(String[] args) {
         Command command = new RegisterCommand("user", "pass");
+        Command command2 = new JoinGameCommand(new Player("name",PlayerColorEnum.BLACK,"123"),"Game1");
         int port = 8080;
-        String ipaddress = "10.24.202.102";
+        String ipaddress = "192.168.1.200";
         CommandManager manager = new CommandManager(ipaddress, port);
         manager.sendCommand(command);
+        manager.sendCommand(command2);
         String nothing = "null";
         Command nextCommand = manager.getCommand();
+        Command nextCommand2 = manager.getCommand();
         String something = "nothing";
     }
 }
