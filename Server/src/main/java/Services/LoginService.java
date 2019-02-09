5545e01f032a8e9e8ca7f9f4ca313715d94f694a
sarah <sarahmykelle@gmail.com>
2019-02-09 14:12:15 -0700

    check right model in loginservice

--- Server/src/main/java/Services/LoginService.java
+++ Server/src/main/java/Services/LoginService.java
@@ -4,22 +4,27 @@ import java.net.UnknownHostException;
 
 import Command.ErrorCommand;
 import Models.*;
+import sun.applet.Main;
+import sun.rmi.runtime.Log;
 
 public class LoginService implements Service {
+    MainModel model;
+
+    public LoginService(){
+        model = MainModel.get();
+    }
     @Override
     public Object doService(Object... obj) {
         String username = (String) obj[0];
         String password = (String) obj[1];
 
         //check that user exists with that username and password
-        UserList users = UserList.get();
-        if(users.findUser(username,password)){
+        if(model.getUserList().findUser(username, password)){
             return true;
         } else{
             try{
                 String localhost = InetAddress.getLocalHost().getHostAddress();
                 return new ErrorCommand("user does not exist", localhost);
-
             } catch(UnknownHostException e){
                 return new ErrorCommand("user does not exist & couldn't get IPAddress", null);
             }
