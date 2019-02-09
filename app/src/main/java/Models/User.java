2f6f389b48f29e24efbd0e5c5b56d7e303d31bbf
sarah <sarahmykelle@gmail.com>
2019-02-09 09:48:00 -0700

    Front-end models

--- app/src/main/java/Models/User.java
+++ app/src/main/java/Models/User.java
@@ -1,5 +1,4 @@
-
-
+package Models;
 
 public class User {
 	//The unique name of this user
@@ -10,11 +9,12 @@ public class User {
 	Boolean loggedIn = false;
 	//The auth token for server verification (if necessary)
 	String authToken;
-	
-	public PlayerColorEnum getColor(){
-		return playerColor;
+
+	public User(String name_in, String password_in){
+		name = name_in;
+		password = password_in;
 	}
-	
+
 	public String getName(){
 		return name;
 	}
@@ -30,17 +30,13 @@ public class User {
 	public String getAuthToken(){
 		return authToken;
 	}
-	
-	public void setColor(PlayerColorEnum input){
-		this.playerColor = input;
-	}
-	
+
 	public void setName(String input){
 		this.name = input;
 	}
 	
 	public void setPassword(String input){
-		this.id = password;
+		this.password = input;
 	}
 	
 	public void setLoggedIn(Boolean state){
---
+++ app/src/main/java/Models/UserList.java
@@ -0,0 +1,29 @@
+package Models;
+
+import java.util.ArrayList;
+
+public class UserList{
+    private ArrayList<User> list;
+    private static final UserList ourInstance = new UserList();
+
+    public static UserList get() {
+        return ourInstance;
+    }
+
+    private UserList() {
+        list = new ArrayList<User>();
+    }
+
+    public void addUser(User newUser){
+        list.add(newUser);
+    }
+
+    public boolean findUser(String username, String password){
+        for(User user : list){
+            if(user.name == username && user.password == password){
+                return true;
+            }
+        }
+        return false;
+    }
+}
