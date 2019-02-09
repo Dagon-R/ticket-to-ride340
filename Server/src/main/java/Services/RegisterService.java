ae03192e27f1ec2c73e0173bd4de89d5f8a27220
sarah <sarahmykelle@gmail.com>
2019-02-09 14:03:07 -0700

    Handle errors in registerservice

--- Server/src/main/java/Services/RegisterService.java
+++ Server/src/main/java/Services/RegisterService.java
@@ -1,22 +1,43 @@
 package Services;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import Command.ErrorCommand;
 import Models.MainModel;
 import Models.User;
 import Models.UserList;
 
 public class RegisterService implements Service {
+    MainModel model;
+
+    public RegisterService(){
+        model = MainModel.get();
+    }
+
     @Override
     //Takes in String username, String password
     public Object doService(Object... obj) {
         //create user model obj and set name, password
         String username = (String) obj[0];
         String password = (String) obj[1];
-//        User newUser = new User(username, password);
-        User newUser = new User(username, password);
-//        MainModel.get().getUserList().findUser()
-        UserList users = UserList.get();
-        users.addUser(newUser);
-        return true;
+
+        if(model.getUserList().findUser(username, password)){
+            try{
+                String localhost = InetAddress.getLocalHost().getHostAddress();
+                return new ErrorCommand("Username is already taken", localhost);
+
+            } catch(UnknownHostException e){
+                return new ErrorCommand("Username is already taken & couldn't get IPAddress", null);
+            }
+        } else{
+            User newUser = new User(username, password);
+            UserList users = UserList.get();
+            users.addUser(newUser);
+            return true;
+
+        }
+
         //not really any way this can fail right now.
     }
 }
