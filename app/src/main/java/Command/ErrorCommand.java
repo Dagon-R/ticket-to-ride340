21435786a7d12da4da6b91d8f1661c2b92bc54a4
ChandlerEady <chandler.eady@gmail.com>
2019-02-09 13:26:33 -0700

    NewlyCommittedCommand

--- app/src/main/java/Command/ErrorCommand.java
+++ app/src/main/java/Command/ErrorCommand.java
@@ -4,14 +4,16 @@ import Services.ErrorService;
 
 public class ErrorCommand implements Command {
     private String message;
+    private String ipAddress;
 
     @Override
     public void execute() {
         ErrorService errorService = new ErrorService();
-        errorService.doService(message);
+        errorService.doService(message, ipAddress);
     }
     public ErrorCommand(String message)
     {
         this.message = message;
+        ipAddress = null;
     }
 }
