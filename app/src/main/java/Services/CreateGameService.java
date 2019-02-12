f147cefe28786c885ffbcebfc1c7cf3639223a5c
sarah <sarahmykelle@gmail.com>
2019-02-12 02:00:15 -0700

    fix error message

--- app/src/main/java/Services/CreateGameService.java
+++ app/src/main/java/Services/CreateGameService.java
@@ -27,9 +27,9 @@ public class CreateGameService implements Service {
     @Override
     public void doService(Object... obj) {
         //Check params
-        if(obj.length != 3){
+        if(obj.length != 4){
             model.setErrorMessage("Error Creating Game");
-            System.out.println("ERROR: " + obj.length + " instead of 3 params on frontend createGame service");
+            System.out.println("ERROR: " + obj.length + " instead of 4 params on frontend createGame service");
         }
 
         System.out.println(obj);
@@ -37,6 +37,7 @@ public class CreateGameService implements Service {
         Player host = (Player) obj[0];
         String gameName = (String) obj[1];
         String ipAddress = (String) obj[2];
+        ClientGameList gameList = (ClientGameList) obj[3];
 
         PendingGame newGame = new PendingGame(host, gameName);
         model.getGameList().addServerPendingGame(newGame);
