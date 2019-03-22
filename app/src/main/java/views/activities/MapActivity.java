f5a80145151d75f69efa2b323afa97f92e183870
sarah <sarahmykelle@gmail.com>
2019-03-22 13:57:43 -0600

    viewsss changed

--- app/src/main/java/views/activities/MapActivity.java
+++ app/src/main/java/views/activities/MapActivity.java
@@ -21,27 +21,30 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.EnumSet;
-import java.util.HashSet;
 import java.util.TreeSet;
 
 import Models.ActiveGame;
+import Models.MainModel;
 import Models.Player;
+import Models.PlayerColorEnum;
 import Phase2Models.ChatQueue;
 import Phase2Models.DestinationCard;
 import Phase2Models.MapModel;
+import Phase2Models.Store;
 import Phase2Models.TrainCardColor;
 import Presenters.MapPresenter;
-import views.Adapters.ChatAdapter;
 import views.Adapters.DestinationAdapter;
 import views.MapLogic;
 import views.PlayerSummaryView;
 import views.R;
+import views.ViewInterfaces.ActionBar;
 import views.ViewInterfaces.IMap;
 import views.ViewInterfaces.MesssageSender;
+import views.Adapters.ChatAdapter;
 
 //import android.support.design.widget.BottomSheetBehavior;
 
-public class MapActivity extends AppCompatActivity implements IMap, MesssageSender { ///
+public class MapActivity extends AppCompatActivity implements ActionBar, IMap, MesssageSender {
     static String TAG = "MapActivity";
 //    MapLogic mapLogic;
     MapLogic mapLogic;
@@ -74,15 +77,16 @@ public class MapActivity extends AppCompatActivity implements IMap, MesssageSend
     PlayerSummaryView player5;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         construct();
-        this.getSupportActionBar().hide();
 
     }
+
 
 
     private void construct() {
@@ -105,8 +109,6 @@ public class MapActivity extends AppCompatActivity implements IMap, MesssageSend
 
     private void setupPlayerDrawer(){
         destinationCardDisplay = findViewById(R.id.destination_card_recycler_view);
-        destinationCardDisplay.setAdapter(new DestinationAdapter(new HashSet<DestinationCard>()));
-        destinationCardDisplay.setLayoutManager(new LinearLayoutManager(this));
         blueCards = findViewById(R.id.blue_card_box);
         redCards = findViewById(R.id.red_card_box);
         yellowCards = findViewById(R.id.yellow_card_box);
@@ -180,11 +182,130 @@ public class MapActivity extends AppCompatActivity implements IMap, MesssageSend
     public void updateGameInfo(ActiveGame game) {
         TreeSet<Player> players = game.getPlayers();
         int i = 0;
-        System.out.println(game);
         for (Player currentPlayer : players){
             playersArray[i].setInfo(currentPlayer);
             i++;
         }
+    }
+
+    public void updateDestDeck(int deckSize){
+        TextView tv = (TextView) findViewById(R.id.destDeckNum);
+        tv.setText(String.valueOf(deckSize));
+    }
+
+    public void updateTrainDeck(int deckSize){
+        TextView tv = (TextView) findViewById(R.id.trainDeckNum);
+        tv.setText(String.valueOf(deckSize));
+    }
+
+    public void updateStore(Store store) {
+        ArrayList<View> views = new ArrayList<>();
+        views.add(findViewById(R.id.store0));
+        views.add(findViewById(R.id.store1));
+        views.add(findViewById(R.id.store2));
+        views.add(findViewById(R.id.store3));
+        views.add(findViewById(R.id.store4));
+
+        TrainCardColor[] storeList = store.getStore();
+        for(int i = 0; i < storeList.length; i++){
+            views.get(i).setBackgroundColor(getTrainColor(storeList[i]));
+        }
+    }
+
+    private int getTrainColor(TrainCardColor cardColor){
+        int retColor = 0;
+        switch (cardColor){
+            case BLUE:
+                retColor = getResources().getColor(R.color.blue);
+                break;
+            case RED:
+                retColor = getResources().getColor(R.color.red);
+                break;
+            case BLACK:
+                retColor = getResources().getColor(R.color.black);
+                break;
+            case GREEN:
+                retColor = getResources().getColor(R.color.green);
+                break;
+            case WHITE:
+                retColor = getResources().getColor(R.color.white);
+                break;
+            case ORANGE:
+                retColor = getResources().getColor(R.color.orange);
+                break;
+            case PURPLE:
+                retColor = getResources().getColor(R.color.purple);
+                break;
+            case YELLOW:
+                retColor = getResources().getColor(R.color.yellow);
+                break;
+            case RAINBOW:
+                retColor = getResources().getColor(R.color.rainbow);
+                break;
+        }
+        return retColor;
+    }
+
+    private int getPlayerColor(PlayerColorEnum playerColor){
+        int retColor = 0;
+        switch (playerColor) { //RBGYB
+            case RED:
+                retColor = getResources().getColor(R.color.p0Color);
+                break;
+            case BLUE:
+                retColor = getResources().getColor(R.color.p1Color);
+                break;
+            case GREEN:
+                retColor = getResources().getColor(R.color.p2Color);
+                break;
+            case YELLOW:
+                retColor = getResources().getColor(R.color.p3Color);
+                break;
+            case BLACK:
+                retColor = getResources().getColor(R.color.p4Color);
+                break;
+        }
+        return retColor;
+    }
+
+
+
+    public void updateTurnView(int playerIndex, TreeSet<Player> playerList) {
+        //grey out i - 1 and color i
+        ArrayList<View> views = new ArrayList<>();
+        views.add(findViewById(R.id.p0Turn));
+        views.add(findViewById(R.id.p1Turn));
+        views.add(findViewById(R.id.p2Turn));
+        views.add(findViewById(R.id.p3Turn));
+        views.add(findViewById(R.id.p4Turn));
+
+        int i = 0;
+        int lastPlayer = playerIndex - 1;
+        if(lastPlayer < 0) lastPlayer = playerList.size();
+        for(Player player : playerList){
+            //set active player view color
+            if (i == playerIndex) views.get(i).getBackground().setTint(getPlayerColor(player.getColor()));
+            //set previous active player view color to grey
+            if(i == lastPlayer)  views.get(i).getBackground().setTint(getResources().getColor(R.color.grey));
+        }
+    }
+
+
+    @Override
+    public void drawStore(int i) {
+        //replace card at i with new dest card
+        mapPresenter.drawStore(i);
+
+    }
+
+    @Override
+    public void drawTrainCard() {
+        mapPresenter.drawTrainCard();
+    }
+
+    @Override
+    public void drawDestinationCard() {
+        mapPresenter.drawDestination();
     }
 
 
@@ -202,8 +323,6 @@ public class MapActivity extends AppCompatActivity implements IMap, MesssageSend
 
         mapPresenter.selectCity(x, y, size);
     }
-
-    //TODO: move everything from here? out to DialogLogic
 
     public void setDialogInfo(EnumSet<DestinationCard> destHand){
         showDestDialog(this, destHand);
