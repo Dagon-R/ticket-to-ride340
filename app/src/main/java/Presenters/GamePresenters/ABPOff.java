d3b7cc7b47736a8495c8b36aac471071dd431139
ChandlerEady <chandler.eady@gmail.com>
2019-03-29 15:19:37 -0600

    Now making toast

--- app/src/main/java/Presenters/GamePresenters/ABPOff.java
+++ app/src/main/java/Presenters/GamePresenters/ABPOff.java
@@ -4,21 +4,30 @@ import android.util.Log;
 import android.view.View;
 
 import States.IActionBarPresenter;
+import views.activities.MapActivity;
 
 public class ABPOff implements IActionBarPresenter {
     private static String TAG = "ActionBarPresenter";
+    private MapActivity activity;
+    ABPOff(MapActivity mapActivity) {this.activity = mapActivity;}
     @Override
     public void drawStore(View view, int i) {
-        Log.d(TAG, "Tried to draw from an inactive store");
+        String STORE_ERROR = "Tried to draw from an inactive store";
+        activity.popToast(STORE_ERROR);
+        Log.d(TAG, STORE_ERROR);
     }
 
     @Override
     public void drawDestinationCard(View view) {
-        Log.d(TAG, "Tried to draw from an inactive destination deck");
+        String DEST_ERROR = "Tried to draw from an inactive destination deck";
+        activity.popToast(DEST_ERROR);
+        Log.d(TAG, DEST_ERROR);
     }
 
     @Override
     public void drawTrainCarCard(View view) {
-        Log.d(TAG, "Tried to draw from an inactive train card deck");
+        String TRAIN_ERROR = "Tried to draw from an inactive train card deck";
+        activity.popToast(TRAIN_ERROR);
+        Log.d(TAG, TRAIN_ERROR);
     }
 }
